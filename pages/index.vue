9e06707daa444c634baa1ebe6c71ec1181d30ad0
Toma1220 <103945900+Toma1220@users.noreply.github.com>
2023-01-09 07:03:51 +0000

    aaa

--- pages/index.vue
+++ pages/index.vue
@@ -3,19 +3,35 @@
     <h1>世界遺産</h1>
     <div class="container mb-5">
       <h2>世界のいろいろな遺産を紹介していきます。</h2>
-<ul class="ul-sekai">
-  <li>タージマハル</li>
-  <li>自由の女神</li>
-  <li>グランドキャニオン</li>
-</ul>
-    </div>
-    </div>
     
+
+    <div class="card mb-3 mx-auto">
+ <h4>タージ・マハル</h4>
+    </div>
+    <img class="ta-zimaharu" src="img/ta-zimaharu.jpg">
+    <a href=”pages/”>タージマハルの魅力をご紹介</a>
+
+    <div class="card mb-3 mx-auto">
+   <h4>自由の女神</h4>
+   </div>
+   <img class="ziyuu-megami" src="img/free-megami.webp">
+    <a href=”pages/”>自由の女神の魅力をご紹介</a>
+
+
+
+   <div class="card mb-3 mx-auto">
+   <h4>ケルン大聖堂</h4>
+   </div>
+   <img class="kerun-daiseido" src="img/kerun-daiseido.jpg">
+     <a href=”pages/”>ケルン大聖堂の魅力をご紹介</a>
+
+
+   </div>
+  </div>
 </template>
 
 
 
 <style>
-
 
 </style>
