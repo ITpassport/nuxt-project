64d2ab1298ca5816a2954f04d9a8af699d853337
Toma1220 <103945900+Toma1220@users.noreply.github.com>
2023-01-17 04:59:13 +0000

    comit

---
+++ pages/batikansikoku.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="container text-center my-5">
+        <h1>バチカン市国</h1>
+    
+        <div class="container mb-5">
+        <img src="img/ta-zimaharu.jpg" class="img-fluid" alt="ta-zimaharu">
+        <p class="fs-3">バチカン市国はイタリアのローマにある世界最小の独立国で国自体が世界遺産に登録されています。
+            また、全世界に約10億人の信者を持つカトリックの総本山でもあります。
+16～17世紀にミケランジェロなどの設計により再建された
+サン・ピエトロ大聖堂、バチカン宮殿などがあり、
+宗教的にだけではなく、
+芸術的にも価値のある場所です
+    </p>
+        </div>
+        </div>
+    </template>
