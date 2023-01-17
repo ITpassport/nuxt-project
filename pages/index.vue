27ceacbb6f64c77bacdbdb7805e24a5e7b706cff
Toma1220 <103945900+Toma1220@users.noreply.github.com>
2023-01-17 04:48:28 +0000

    comitcomit

--- pages/index.vue
+++ pages/index.vue
@@ -9,26 +9,26 @@
  <h4>タージ・マハル</h4>
     </div>
     <img class="ta-zimaharu" src="img/ta-zimaharu.jpg">
-    <a href=”pages/”>タージマハルの魅力をご紹介</a>
+    <a href="pages/ta-zimaharu.vue">タージマハルの魅力をご紹介</a>
 
     <div class="card mb-3 mx-auto">
    <h4>自由の女神</h4>
    </div>
    <img class="ziyuu-megami" src="img/free-megami.webp">
-    <a href=”pages/”>自由の女神の魅力をご紹介</a>
-  
+    <a href="pages/free-megami.vue">自由の女神の魅力をご紹介</a>
+
 
    <div class="card mb-3 mx-auto">
    <h4>ケルン大聖堂</h4>
    </div>
    <img class="kerun-daiseido" src="img/kerun-daiseido.jpg">
-     <a href=”pages/”>ケルン大聖堂の魅力をご紹介</a>
+     <a href="pages/kerun-daiseido.vue">ケルン大聖堂の魅力をご紹介</a>
 
      <div class="card mb-3 mx-auto">
      <h4>バチカン市国</h4>
      </div>
      <img class="batikann" src="img/vatican_3.jpg">
-     <a href="page/">バチカン市国の魅力を紹介します</a>
+     <a href="pages/batikansikoku.vue">バチカン市国の魅力を紹介します</a>
 
 
      <footer></footer>
