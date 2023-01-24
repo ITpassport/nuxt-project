49aed167a49d02837e80e604ed30757ae671d0d1
Toma1220 <103945900+Toma1220@users.noreply.github.com>
2023-01-24 05:52:16 +0000

    comit push

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -17,5 +17,5 @@ export default defineNuxtConfig({
     modules: [
         '@vueuse/nuxt',
         '@nuxt/content'
-      ],
+    ],
 })
--- pages/free-megami.vue
+++ pages/free-megami.vue
@@ -1,4 +1,31 @@
 <template>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<div class="container-fluid">
+    <a class="navbar-brand" href="#">world heritage</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+        <li class="nav-item">
+        <NuxtLink class="nav-link active" aria-current="page" href="/ta-zimaharu">タージマハル</NuxtLink>
+        </li>
+        <li class="nav-item">
+        <NuxtLink class="nav-link" href="/free-megami">自由の女神</NuxtLink>
+        </li>
+        <li class="nav-item">
+        <NuxtLink class="nav-link" href="/kerun-daiseido">ケルン大聖堂</NuxtLink>
+        </li>
+        <li class="nav-item">
+        <NuxtLink class="nav-link" href="/batikansikoku" tabindex="-1" aria-disabled="true">バチカン市国</NuxtLink>
+        </li>
+        <li class="nav-item">
+        <NuxtLink class="nav-link" href="/index">ホームに戻る</NuxtLink>
+        </li>
+    </ul>
+    </div>
+</div>
+</nav>
     <div class="container text-center my-5">
         <h1>自由の女神</h1>
         <div class="container mb-5">
